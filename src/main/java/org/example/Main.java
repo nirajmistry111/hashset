e1d01562d80a244415b59c4c4a1c01bbd0dbf5c3
Niraj Mistry <niraj.mistry@dcmail.ca>
2023-06-27 17:09:16 -0500

    “Initial commit”

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Date;
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println(d);
+        System.out.println(d.getTime());
+
+
+    }
+}
